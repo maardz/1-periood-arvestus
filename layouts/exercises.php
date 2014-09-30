79bebf209dbf9f9eb3c32a699a0e2568046e3236
maardz <moldermarge@gmail.com>
2014-09-30 22:26:02 +0300

    Add ÜL 4

--- layouts/exercises.php
+++ layouts/exercises.php
@@ -11,13 +11,29 @@
     <main id="site-body">
       <section id="site-content">
         <h2>Tingimuslause</h2>
-        <!--  Sisesta siia kood -->
+        <?php  
 
+          $rand_nr=rand(100,200);
+
+          if($rand_nr>150){
+            echo 'Juhuslik number "'.$rand_nr.'" on suurem kui 150';
+          }else if($rand_nr<150){
+            echo 'Juhuslik number "'.$rand_nr.'" on väiksem kui 150';
+          }else {
+            echo 'Juhuslik number on "'.$rand_nr.'"';
+          }
+
+        ?>
         <h2>Juhuslik pilt</h2>
-        <!--  Sisesta siia kood -->
-
+        <?php $rand_img=rand(1,12); ?>
+        <img src="http://users.khk.ee/kaspar.naaber/svp/periood-1/arvestus/images/<?php echo $rand_img; ?>.jpg">
         <h2>Kõikide piltide väljastamine</h2>
-        <!--  Sisesta siia kood -->
+        <?php 
+          for ($i=1; $i <=12 ; $i++) { ?>
+            <img src="http://users.khk.ee/kaspar.naaber/svp/periood-1/arvestus/images/<?php echo $i; ?>.jpg">
+        <?php
+          }
+        ?>
       </section>
     </main>
 
